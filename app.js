1482f3a8c330a7cc6874de0da3c74168dd760a1b
Travis F <agathangalos@gmail.com>
2022-01-12 13:37:38 -0500

    Remove book button added, works as intended.

--- app.js
+++ app.js
@@ -10,6 +10,52 @@ function Book(title, author, genre, publisher, readStatus) {
     this.readStatus = readStatus;
 }
 
+Book.prototype.changeStatus = function() {
+    
+}
+
+const eventHandler = {
+    handlers: {
+        click(e) {
+            if (e.currentTarget.innerText = "Remove From Library") {
+                let attribute = e.currentTarget.parentNode.getAttribute('data-index');
+                console.log(attribute);
+                console.log('----');
+                bookArr.forEach((book) => console.log(book.index));
+                //console.log(bookArr[attribute].index);
+
+                // removes the div card from the DOM
+                let x = e.currentTarget.parentNode;
+                x.remove(x);
+
+                // removes the corresponding Book object from bookArr
+                let y = bookArr.indexOf(bookArr[attribute]);
+                bookArr.splice(y, 1);
+
+                /* 
+                    this method so far works in all cases but it does eventually
+                    'unlink' the actual array index of the object and it's 
+                    corresponding div card's data-index attribute if 'books' are
+                    made and 'deleted' from the 'library'.
+
+                    I think this could be corrected somehow, but it does not 
+                    currently interfere with the functioning of the program so
+                    I'm just going to let it remain as is for now. 
+                */
+            }
+        },
+        keydown(e) {
+
+        },
+    },
+    handleEvent(e) {
+        switch (e.type) {
+            case 'click':
+                this.handlers.click(e);
+        }
+    },
+}
+
 /* 
 pushes new books to the Book object and generates their display 
 on the page when called by the form event handler
@@ -20,9 +66,11 @@ function addBook(a, b, c, d, e) {
 
     // creates the div 'cards' and their indices
     let div = document.createElement('div');
+    let button = document.createElement('button');
     let index = bookArr.indexOf(book);
-    document.createAttribute('data-index');
+
     div.setAttribute('id', 'card');
+    document.createAttribute('data-index');
     div.setAttribute('data-index', index);
     cards.appendChild(div);
 
@@ -32,6 +80,17 @@ function addBook(a, b, c, d, e) {
         p.innerText = book[prop];
         div.appendChild(p);
     };
+
+    // add buttons to the cards;
+    button.setAttribute('id', 'remove_book_button');
+    button.innerText = "Remove From Library";
+    div.appendChild(button);
+    button.addEventListener('click', eventHandler);
+
+    // adds a new key:value to the Book object based on the 
+    // data-index value of the book card being created
+    book.index = index;
+
 }
 
 // test values
